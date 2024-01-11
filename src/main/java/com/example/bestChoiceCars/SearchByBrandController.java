7e9502ae1d72828cb63131308d6c45cff6068869
lucasnpc <lucasnpc2@outlook.com>
2024-01-11 17:07:19 -0300

    creating search by brand screen

--- src/main/java/com/example/bestChoiceCars/SearchByBrandController.java
+++ src/main/java/com/example/bestChoiceCars/SearchByBrandController.java
@@ -1,12 +1,23 @@
 package com.example.bestChoiceCars;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.bestChoiceCars.repositories.CarsRepository;
 
 @Controller
 public class SearchByBrandController {
+
+    @Autowired
+    CarsRepository carsRepository;
+    
     @RequestMapping("/searchByBrand")
-    public String searchByBrandScreen() {
+    public String searchByBrandScreen(Model model) {
+        var brands = carsRepository.findDistinctBrands();
+        model.addAttribute("brands", brands);
+        
         return "search-by-brand";
     }
 }
--- src/main/java/com/example/bestChoiceCars/repositories/CarsRepository.java
+++ src/main/java/com/example/bestChoiceCars/repositories/CarsRepository.java
@@ -1,13 +1,18 @@
 package com.example.bestChoiceCars.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.bestChoiceCars.model.Car;
 
 @Repository
 public interface CarsRepository extends JpaRepository<Car, UUID> {
+
+    @Query("SELECT DISTINCT c.brand FROM Car c")
+    List<String> findDistinctBrands();
 
 }
